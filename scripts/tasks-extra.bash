9014124a0b994b15cfbb5cae1220726d2f51951e
Ciprian Dorin Craciun <ciprian@volution.ro>
2015-10-21 21:50:56 +0300

    Remove `mosaic-node-debugging` package

--- scripts/tasks-extra.bash
+++ scripts/tasks-extra.bash
@@ -5,12 +5,6 @@ cat <<EOS
 mosaic-platform-core@package : mosaic-node-boot@package
 mosaic-platform-java@package : mosaic-node-boot@package
 
-mosaic-platform-core@publish :
-mosaic-platform-java@publish :
-
-mosaic-node-debugging@package :
-mosaic-node-debugging@publish :
-
 EOS
 
 exit 0
